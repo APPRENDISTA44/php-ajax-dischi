44f188b60d4cb11a49fb7b49b314ff42d7ceb036
Luca Isidoro <apprendista44@gmail.com>
2020-07-16 16:55:46 +0200

    esercizio completo

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@
     foreach ($database as $disco) {
       $output .= '<div class = "album">' . '<img src="' . $disco['poster'] . '" alt="logo">';
       $output .= '<h2>' .  $disco['title'] . '</h2>';
-      $output .= '<h4>' . $disco['author'] . '</h4>';
-      $output .= '<h4>' . $disco['year'] . '</h4>' . '</div>';
+      $output .= '<h3>' . $disco['author'] . '</h3>';
+      $output .= '<h3>' . $disco['year'] . '</h3>' . '</div>';
     }
     return $output;
   }

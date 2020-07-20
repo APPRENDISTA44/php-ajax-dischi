d86500ee6502461c154b9e193ac1d3f75f9d11f2
Luca Isidoro <apprendista44@gmail.com>
2020-07-20 18:44:05 +0200

    commit

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="dist/app.css">
     <title>dischi</title>
   </head>
+
   <body>
 
     <!-- header -->
     <header>
       <img src="img/logo-small.svg" alt="logo">
         <select>
-          
+
         </select>
     </header>
     <!--fine header -->
